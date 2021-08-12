c08552a3d454a3d30cb010539574e2790fef2a73
JudithMishel <frontend2@medialabla.com>
2021-08-12 17:53:52 -0500

    add new changes in global.js

--- CLOSURES/src/scope/global.js
+++ CLOSURES/src/scope/global.js
@@ -1,1 +1,27 @@
-var hello = "hello"
+//Global Scope : No están dentro de funciones o bloques, por lo tanto se puede acceder a ellas de manera global.
+
+var hello = "Hello"; //VAR tiene scope global
+var hello = "Hello+"; //VAR si puede ser reasignada pero es una mala práctica
+let world = "Hello World";
+//let world = "Hello World"; Genera error porque no puede ser reasignado
+const helloWorld = "Hello World";
+
+const anotherFunction = () => {
+    console.log(hello);
+    console.log(world);
+    console.log(helloWorld);
+}
+
+anotherFunction()
+
+/*
+
+Es una mala practica realizar esto
+
+const newWorld = () => {
+    world1 = "Nice to meet you"
+}
+
+newWorld()
+console.log(world1); 
+*/
